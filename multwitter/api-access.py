174a8e7f2855756df32eae6d23bc46493ab81d07
Exaudeus <etiennebeaulac@gmail.com>
2017-11-16 16:32:35 -0700

    Blah blah

--- multwitter/api-access.py
+++ multwitter/api-access.py
@@ -2,9 +2,17 @@ from twython import Twython
 import json
 
 
-with open('secret.json') as f:
+with open('secret.json', 'r') as f:
     data = json.load(f)
 
 twitter = Twython(data['APP_KEY'], data['APP_SECRET'])
-
 auth = twitter.get_authentication_tokens()
+
+results = twitter.cursor(twitter.search, q='Python')
+# print(len(results))
+# for result in results:
+#     print(result['user'])
+count = 0
+for result in results:
+    count += 1
+    print(count)
