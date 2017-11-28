2268adb7ffb6ec7e1b8f817752de7085bc399367
JohnMWood <jmw.home@gmail.com>
2017-11-28 15:40:50 -0700

    Commit

--- multwitter/api-access.py
+++ multwitter/api-access.py
@@ -2,9 +2,12 @@ from twython import Twython
 import json
 
 
-with open('secret.json') as f:
+with open('secret.json', 'r') as f:
     data = json.load(f)
 
 twitter = Twython(data['APP_KEY'], data['APP_SECRET'])
-
 auth = twitter.get_authentication_tokens()
+
+results = twitter.cursor(twitter.search, q='Python')
+for result in results:
+    print(result)
